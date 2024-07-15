5349f6fff45b642bfd22d2328ca1f68c9b408999
kilkellym <michael@archsmarter.com>
2024-07-15 16:12:26 -0400

    Add project files.

---
+++ RAB_Orientation_July24/Common/CommandAvailability.cs
@@ -0,0 +1,17 @@
+﻿namespace RAB_Orientation_July24.Common
+{
+	internal class CommandAvailability : IExternalCommandAvailability
+	{
+		public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+		{
+			bool result = false;
+			UIDocument activeDoc = applicationData.ActiveUIDocument;
+			if (activeDoc != null && activeDoc.Document != null)
+			{
+				result = true;
+			}
+
+			return result;
+		}
+	}
+}
